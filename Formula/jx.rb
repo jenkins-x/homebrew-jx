8002c8a14b51c1e9fce9af505615474e46e4ace2
rajdavies <rajdavies@gmail.com>
2018-01-10 05:21:36 +0000

    use tar of executable to download

--- Formula/jx.rb
+++ Formula/jx.rb
@@ -1,8 +1,9 @@
 class Jx < Formula
   desc "A tool to install and interact with Jenkins X on your Kubernetes cluster."
   homepage "https://jenkins-x.github.io/jenkins-x-website/"
-  version "1.0.0"
-  url "https://github.com/jenkins-x/jx/releases/download/v#{version}/jx-darwin-amd64"
+  version "1.0.1"
+
+  url "https://github.com/jenkins-x/jx/releases/download/v#{version}/jx-darwin-amd64.tgz"
   sha256 "26cc0c3b9cb79632448063f2fd95d9b2c61b3720f7596c713f6f8d9882d6e454"
 
   def install
