4270c4b410b1368171378e71beb2cce4454d9428
rajdavies <rajdavies@gmail.com>
2018-01-10 14:29:09 +0000

    use tar.gz file

--- Formula/jx.rb
+++ Formula/jx.rb
@@ -3,7 +3,7 @@ class Jx < Formula
   homepage "https://jenkins-x.github.io/jenkins-x-website/"
   version "1.0.1"
 
-  url "https://github.com/jenkins-x/jx/releases/download/v#{version}/jx-darwin-amd64.tgz"
+  url "https://github.com/jenkins-x/jx/releases/download/v#{version}/jx-darwin-amd64.tar.gz"
   sha256 "7e6c69bb2cabed53b3feeb33c767c2f5a7916205e4b338b7648437d7a5b11ae7"
 
   def install
