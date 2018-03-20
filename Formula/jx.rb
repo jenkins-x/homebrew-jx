ab96c0968b51b3475d27172a8195e3dce014f011
James Rawlings <rawlingsj80@gmail.com>
2018-03-20 09:43:35 +0000

    Manual upgrade jx until pipeline is fixed

--- Formula/jx.rb
+++ Formula/jx.rb
@@ -1,10 +1,10 @@
 class Jx < Formula
   desc "A tool to install and interact with Jenkins X on your Kubernetes cluster."
   homepage "https://jenkins-x.github.io/jenkins-x-website/"
-  version "1.1.10"
+  version "1.1.11"
    
   url "https://github.com/jenkins-x/jx/releases/download/v#{version}/jx-darwin-amd64.tar.gz"
-  sha256 "46501589946dcd44f19d8a9e48e7886c6b64b1046ecf1444d07206bc140a1b36"
+  sha256 "e6b78a473b2bce161e78a092bd74a84f6b796d197cf7ef162c2f74ded816733a"
 
   def install
     bin.install name
