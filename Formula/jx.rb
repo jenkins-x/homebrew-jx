d2ee13099a4b9a118b4d896f70a033c089552bfa
James Strachan <james.strachan@gmail.com>
2019-03-30 07:47:36 +0000

    Update jx.rb

--- Formula/jx.rb
+++ Formula/jx.rb
@@ -1,7 +1,7 @@
 class Jx < Formula
   desc "A tool to install and interact with Jenkins X on your Kubernetes cluster."
   homepage "https://jenkins-x.github.io/jenkins-x-website/"
-  version "1.3.1061"
+  version "1.3.1056"
    
   url "https://github.com/jenkins-x/jx/releases/download/v#{version}/jx-darwin-amd64.tar.gz"
   sha256 "544cffb22da45f3be47039735ee2a26261a70a6033878081b2dd99f3abbcea6b"
