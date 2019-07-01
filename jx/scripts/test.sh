152da8cc0d06e142c5b560f6c7120a7f698fc468
Gareth Evans <gareth@bryncynfelin.co.uk>
2019-07-01 13:34:37 +0100

    fix: using sha256sum

--- jx/scripts/test.sh
+++ jx/scripts/test.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 VERSION=$(cat Formula/jx.rb | grep "version \"" | sed 's/version//' | sed 's/"//g' | tr -d '[:space:]')
 echo $VERSION
@@ -10,4 +10,4 @@ mkdir -p build
 
 cd build
 curl -f -L -o jx-darwin-amd64.tar.gz https://github.com/jenkins-x/jx/releases/download/v${VERSION}/jx-darwin-amd64.tar.gz
-echo "$SHA  jx-darwin-amd64.tar.gz" | shasum -c
+echo "$SHA  jx-darwin-amd64.tar.gz" | sha256sum -c
