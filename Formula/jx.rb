d6c5801a6e06c9b3686182b1371e171eecd608ed
PatrickRiegler <patrick.riegler@intdev.ch>
2021-07-08 10:45:11 +0200

    also update sha256 along with version

--- Formula/jx.rb
+++ Formula/jx.rb
@@ -5,7 +5,7 @@ class Jx < Formula
   version "2.1.155"
 
   url "http://github.com/jenkins-x/jx/releases/download/v#{version}/jx-darwin-amd64.tar.gz"
-  sha256 "c151efdc8b5f95f3b588707c9425dfe1fbfb5c22a5f21650580ee6dbd37a9a42"
+  sha256 "e089b947c213823edb9b24660f37c68f42043d92f0d806dea91d29968357262d"
 
   def install
     bin.install name
